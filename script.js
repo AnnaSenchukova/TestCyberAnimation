05343d47e4e21bdcf4b8b3a517a54731916a11c7
AnnaSenchukova <36882792+AnnaSenchukova@users.noreply.github.com>
2019-03-08 21:47:40 +0300

    js: add script

--- script.js
+++ script.js
@@ -1,31 +1,91 @@
 /*Скрипт печатной машинки*/
-    var htmlStringCyber = document.querySelector('.promo__title--cyber');
-    var htmlStringDay = document.querySelector('.promo__title--decoration');
+var htmlStringCyber = document.querySelector('.promo__title--cyber');
+var htmlStringDay = document.querySelector('.promo__title--decoration');
+htmlStringDay.style.visibility = 'hidden';
+htmlStringCyber.style.visibility = 'hidden';
+
+/*
+TypeWriter.prototype = {
+    element: null,
+    type: function () {
+
+    }
+
+}{
+
+}
+
+var TypeWriter = function (element) {
 
 
-function typewriter(source) {
-    var cloneHtmlString = source.cloneNode(true);
+}
+*/
+
+function typewriter(sourceElement, callback) {
+
+    var isTyped = false;
+
+    var cloneHtmlString = sourceElement.cloneNode(true);
     console.log(cloneHtmlString);
-    source.innerHTML = '';
+    sourceElement.innerHTML = '';
+    sourceElement.style.visibility = 'visible';
 
-    var textString = cloneHtmlString.innerText;//находим нужную строку в клоне и вытаскиваем из неё текст
+    var textString = cloneHtmlString.innerText.trim();//находим нужную строку в клоне и вытаскиваем из неё текст
     console.log(textString);
     var arrayLetters = textString.slice(); //делим текст из клона на по-буквенный массив
 
     var i = 0;
-    (function() {
+    (function writter() {
         if (i < arrayLetters.length) {
-            source.innerHTML += arrayLetters[i];
+            sourceElement.innerHTML += arrayLetters[i];
             i++;
-            setTimeout(arguments.callee, 200);
+            setTimeout(arguments.callee, 400);
+        } else {
+            if (callback) {
+                callback();
+            }
         }
     })();
+
+    /*Скрипт мерцающего курсора
+    function blinkCursor() {
+        setInterval(
+            function blinkOn() {
+                blinkerSourceElement.style.borderRightColor = '#4b6ecd';
+            },
+            200
+        );
+
+        setInterval(
+            function blinkOff() {
+                blinkerSourceElement.style.borderRightColor = 'transparent';
+
+            }, 400);
+    }
+
+    setTimeout(blinkCursor, 600);
+
+*/
+
 }
 
 
-    typewriter(htmlStringCyber);
-    setTimeout(typewriter, 1500, htmlStringDay);
 
+function typeMonday() {
+    typewriter(htmlStringDay);
+    htmlStringDay.style.width = 'auto';
+    htmlStringDay.classList.add('blinking-cursor-class');
+    htmlStringCyber.classList.remove('blinking-cursor-class');
+}
+
+
+typewriter(
+    htmlStringCyber,
+    typeMonday
+);
+
+
+/*setTimeout(typewriter, 12200, htmlStringDay);*/
 
 
 /* Скрипт появления/изчезновения текста */
@@ -37,23 +97,27 @@ function visiability(hiddenObject) {
     var hiddenText = cloneHtml.innerText;
     hiddenObject.innerHTML = '';
 
-    setTimeout( function () {hiddenObject.innerHTML = hiddenText;}, 15000);
+    setTimeout(function () {
+        hiddenObject.innerHTML = hiddenText;
+    }, 23000);
 }
 
 visiability(htmlNote);
 
 /* Скрипт заполнения строки */
 
-
+var htmlPreload = document.querySelector('.preload__wrapper');
+htmlPreload.style.visibility = 'hidden';
 
 function progress() {
+    htmlPreload.style.visibility = 'visible';
     document.querySelector('.preload__value--decades').remove();
     document.querySelector('.preload__value--units').remove();
 
     var counterProgress = 0;
     var counterPercentages = 0;
-    var timer = setInterval( preloadProgress, 100 );
-    
+    var timer = setInterval(preloadProgress, 100);
+
     function preloadProgress() {
         var htmlPreloadLine = document.querySelector('.preload__progress');
 
@@ -71,9 +135,10 @@ function progress() {
             counterPercentages = counterProgress;
             htmlPercentage.innerHTML = counterPercentages + '%';
         }
-        
+
         percentagesProgress();
     }
+
 }
 
-progress();
+setTimeout(progress, 15000);
